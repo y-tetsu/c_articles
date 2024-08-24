bfbaae94172af102208b5ae924d39627bf10ffe9
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2024-08-24 21:52:38 +0900

    add freed

---
+++ ub/freed/freed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr = (int *)malloc(sizeof(int));
+
+    *ptr = 569;
+    printf("before : %d\n", *ptr);
+
+    free(ptr);
+
+    printf("after  : %d\n", *ptr);
+
+    return 0;
+}
