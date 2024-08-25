8ce10c665d525d97e19644dde6604031170c3391
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2024-08-25 12:45:12 +0900

    fix code

--- ub/freed/freed.c
+++ ub/freed/freed.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     int *ptr = (int *)malloc(sizeof(int));
 
     *ptr = 569;
     printf("before : %d\n", *ptr);
 
     free(ptr);
-
     printf("after  : %d\n", *ptr);
 
     return 0;
